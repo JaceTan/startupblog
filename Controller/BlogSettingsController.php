9189d77af1bc42bda0dfa3fb658c54aff089dd11
Jace <jacetan93@gmail.com>
2013-04-17 12:01:30 +0800

    added BlogSettings controller

---
+++ Controller/BlogSettingsController.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('StartupBlogAppController', 'StartupBlog.Controller');
+/**
+ * BlogSettings Controller
+ *
+ */
+class BlogSettingsController extends StartupBlogAppController {
+
+/**
+ * Scaffold
+ *
+ * @var mixed
+ */
+	public $scaffold;
+
+}
---
+++ Test/Case/Controller/BlogSettingsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+App::uses('BlogSettingsController', 'StartupBlog.Controller');
+
+/**
+ * BlogSettingsController Test Case
+ *
+ */
+class BlogSettingsControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.startup_blog.blog_setting'
+	);
+
+}
